763c431df01db72e21d3328ead828cd0eab24b82
15520065 <15520065@gm.uit.edu.vn>
2018-06-02 16:42:29 +0700

    Menu

--- desktop/src/com/gdx/game2048/desktop/DesktopLauncher.java
+++ desktop/src/com/gdx/game2048/desktop/DesktopLauncher.java
@@ -4,12 +4,13 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.gdx.game2048.GameScreen;
 import com.gdx.game2048.GameView;
+import com.gdx.game2048.MainMenuView;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = 480;
 		config.height = 800;
-		new LwjglApplication(new GameView(), config);
+		new LwjglApplication(new MainMenuView(), config);
 	}
 }
